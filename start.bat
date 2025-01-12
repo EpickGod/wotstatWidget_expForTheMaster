2c789a2977b70c70edb8777325612019f5f25306
EpickGod <freelance.lab@yandex.ru>
2025-01-13 02:41:46 +0300

    Удалены файлы из репозитория

--- start.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starting backend...
-start cmd /k "cd D:\reactapp\my-react-app\backend && $env:DEBUG='server' && node server.js"
-
-echo Starting frontend...
-start cmd /k "cd D:\reactapp\my-react-app\my-react-app && npm start"
-
-echo All servers started!
